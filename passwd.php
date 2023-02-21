96169538a43c040eb60aec5e2a46971bfc182730
Raphael Hug <102741446+Raphi93@users.noreply.github.com>
2023-02-21 23:48:37 +0100

    Add files via upload

---
+++ passwd.php
@@ -0,0 +1,33 @@
+<?php
+// Verbindung zur Datenbank herstellen
+$servername = "awagadif.mysql.db.internal";
+$username = "awagadif_admin";
+$password = "GFbbKQ=hXY2R9R7H5RU!";
+$dbname = "awagadif_admin";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if (date('w') == 0) {
+    // Passwort generieren
+    $new_password = bin2hex(random_bytes(8));
+
+    // Passwort in der Datenbank aktualisieren
+    $sql = "UPDATE passwords SET password = '$new_password' WHERE id = 1";
+    if ($conn->query($sql) === TRUE) {
+        echo "Password updated successfully";
+    } else {
+        echo "Error updating password: " . $conn->error;
+    }
+
+    // Passwort per E-Mail senden
+    $to = "hug.raphael@gmx.ch";
+    $subject = "Neues Passwort";
+    $message = "Das neue Passwort lautet: $new_password";
+    $headers = "From: raphael.hug@gmx.net";
+    mail($to, $subject, $message, $headers);
+    }
+
+$conn->close();
+?>
---
+++ pdf.php
@@ -0,0 +1,33 @@
+<?php
+// Verbindung zur Datenbank herstellen
+$servername = "awagadif.mysql.db.internal";
+$username = "awagadif_admin";
+$password = "GFbbKQ=hXY2R9R7H5RU!";
+$dbname = "awagadif_admin";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Passwort aus der Datenbank abrufen
+$sql = "SELECT password FROM passwords ORDER BY id DESC LIMIT 1";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $password = $row["password"];
+} else {
+    $password = "";
+}
+
+// Überprüfen, ob das eingegebene Passwort korrekt ist
+if (isset($_POST['password']) && $_POST['password'] == $password) {
+    header('Content-type: application/pdf');
+    readfile('data/Zeugnisse_Raphael_Hug_202211.pdf');
+} else {
+    echo 'Incorrect password';
+}
+
+$conn->close();
+?>
